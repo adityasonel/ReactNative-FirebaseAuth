3062872178372a369fb36eb707eed99284dc4161
Heisen-Berg <aditya.sonel@gmail.com>
2018-04-28 11:54:05 +0530

    Update MyFirebaseConfig.js

--- MyFirebaseConfig.js
+++ MyFirebaseConfig.js
@@ -1,10 +1,10 @@
 const MyFirebaseConfig = {
-    apiKey: "AIzaSyAYaXEfh3yZvZeZYIVJH8Q9U6Bu4IhKbs4",
-    authDomain: "alien-dev-13.firebaseapp.com",
-    databaseURL: "https://alien-dev-13.firebaseio.com",
-    projectId: "alien-dev-13",
-    storageBucket: "alien-dev-13.appspot.com",
-    messagingSenderId: "580643791568"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "AUTH_DOMAIN",
+    databaseURL: "DATABASE_URL",
+    projectId: "PROJECT_ID",
+    storageBucket: "STORAGE_BUCKET",
+    messagingSenderId: "MESSAGING_SENDER_ID"
 } 
 
 export default MyFirebaseConfig;
